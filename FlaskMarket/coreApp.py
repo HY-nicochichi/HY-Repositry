fbca5878b33654b4f813491bfdd39cc465f84088
H Y <PCheepon@outlook.jp>
2023-12-04 11:03:45 +0900

    minor_change

--- FlaskMarket/coreApp.py
+++ FlaskMarket/coreApp.py
@@ -6,14 +6,14 @@ from views1 import bp1
 from views2 import bp2
 from orm import db
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 app.config.from_pyfile('settings.py')
 
 app.register_blueprint(bp1)
 app.register_blueprint(bp2)
 
-talisman = Talisman(app, force_https=False, session_cookie_secure=False)
+# security_header = Talisman(app)
 csrf_protect = CSRFProtect(app)
 server_side_session = Session(app)
 
