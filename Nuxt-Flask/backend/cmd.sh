16e4892f4d525d54d98bf0bdeba37556514fe7c5
HY-nicochichi <138847666+HY-nicochichi@users.noreply.github.com>
2026-02-09 02:46:59 +0900

    Delete Nuxt-Flask directory

--- Nuxt-Flask/backend/cmd.sh
+++
@@ -1,4 +0,0 @@
-pytest tests/test_jwt.py
-pytest tests/test_user.py
-
-gunicorn core:app -b :5000 -k gevent -w 10
--- Nuxt-Flask/backend/flaskapi/models/model_jwt.py
+++
@@ -1,8 +0,0 @@
-from pydantic import (
-    BaseModel,
-    Field
-)
-
-class JWTPost(BaseModel):
-    mail: str = Field(min_length=1)
-    password: str = Field(min_length=1)
